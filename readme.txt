d912a38ca9a5a38e1b7a348e0b0eccab390ca9eb
jamalk <jamalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-18 17:59:48 +0000

    git-svn-id: https://plugins.svn.wordpress.org/syndication-widget/trunk@291088 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,15 +1,17 @@
 === syndication-widget ===
 Contributors: jamalk
-Donate link: http://www.krishe.com/
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=NR7SVN2HFJXX4&lc=GB&item_name=Donation%20for%20Plugin%20Development&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
 Tags: syndication,OPML, ATOM, RSS, feed link,widget
 Requires at least: 2.7
-Tested up to: 3
-Stable tag: 1.0
+Tested up to: 3.01
+Stable tag: 1.1
 
 Adds a sidebar widget that display OPML, ATOM, RSS feed link for your WordPress blog.
 == Description ==
 
 Adds a sidebar widget and display OPML, ATOM, RSS feed link for your WordPress blog.
+For more information about the plugin please see: http://www.krishe.com/en/syndication-widget
+
 == Installation ==
 
 1. Upload `syndication-widget` directory to the `/wp-content/plugins/` directory
@@ -25,12 +27,9 @@ It provides feed link of OPML, ATOM, RSS
 2. Widget setting.
 
 == Changelog ==
+= 1.1 =
+* Update php code open function to class function 
+* Update suppor and donation link
 
 = 1.0 =
-* Initial version
-
-== Please visit ==
-http://wordpress.org/extend/plugins/my-comment/
-http://wordpress.org/extend/plugins/registration-login/
-http://wordpress.org/extend/plugins/admin-css/
-http://wordpress.org/extend/plugins/block-comment/
+* Initial versiono
--- syndication_widget.php
+++ syndication_widget.php
@@ -4,8 +4,8 @@ Plugin Name: syndication-widget
 Plugin URI: http://wordpress.org/extend/plugins/syndication-widget/
 Description: Adds a sidebar widget and display OPML, ATOM, RSS feed link for your WordPress blog.
 Author: Md. Jamal Hossain Khan
-Version: 1.0
-Author URI: http://www.krishe.com/
+Version: 1.1
+Author URI: http://www.krishe.com/en
 
   
 	Copyright 2010 Md. Jamal Hossain Khan (email : jamal_khanbd@yahoo.com)
@@ -25,16 +25,35 @@ Author URI: http://www.krishe.com/
 */
 
 ### Syndication widget
+// Begin Syndication widget plugin
 
-function syndication_widget($args){ 
+$jk_sw = new Syndication_widget();
+
+class Syndication_widget{
+	
+	public function __construct()
+	{
+	add_filter('plugin_row_meta', array(&$this,'jk_filter_plugin_links'), 10, 2);
+	register_sidebar_widget('Syndication', array(&$this,'syndication_widget'));
+	register_widget_control('Syndication', array(&$this,'syndication_widget_options'), 350, 120);
+	}
+	// Add donate and support information
+	function jk_filter_plugin_links($links, $file)
+	{
+		if ( $file == plugin_basename(__FILE__) )
+		{
+		$links[] = '<a href="http://www.krishe.com/en/syndication-widget">' . __('Support') . '</a>';
+		$links[] = '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=NR7SVN2HFJXX4&lc=GB&item_name=Donation%20for%20Plugin%20Development&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted">' . __('Donate') . '</a>';
+		}
+		return $links;
+	}
+
+	function syndication_widget($args)
+	{ 
 		extract($args);
-
 		$options = get_option('syndication_widget');
-
 		$title = htmlspecialchars(stripslashes($options['title']));
-
 		echo $before_widget.$before_title.$title.$after_title;
-
 				echo "<ul>\n";
 					echo "<li>\n";
 					echo "<a  title='RSS Feed' href='".get_option( 'siteurl' )."/?feed=rss2'/>RSS</a>";
@@ -46,38 +65,25 @@ function syndication_widget($args){
 					echo "<a title='POML Feed' href='".get_option( 'siteurl' )."/wp-links-opml.php'/>OPML</a>";
 					echo "</li>\n";
 				echo "</ul>\n";
-		
 		echo $after_widget;
-
-}
+	}
 
 
 	### Syndication Widget Options
 	
-	function syndication_widget_options() {
-
+	function syndication_widget_options() 
+	{
 		$options = get_option('syndication_widget');
-
 		if (!is_array($options)) {
-
 			$options = array('title' => __('Syndication'));
-
 		}
-
 		if ($_POST['syndication_widget-submit']) {
-
 			$options['title'] = strip_tags($_POST['syndication_widget-title']);
-
 			update_option('syndication_widget', $options);
-
 		}
-
 		echo '<p style="text-align: left;"><label for="syndication_widget-title">'.__('Widget Title').':</label>&nbsp;&nbsp;&nbsp;<input type="text" id="syndication_widget-title" name="syndication_widget-title" value="'.htmlspecialchars(stripslashes($options['title'])).'" />';
-
 		echo '<input type="hidden" id="syndication_widget-submit" name="syndication_widget-submit" value="1" />';
-
 	}
-register_sidebar_widget('Syndication', 'syndication_widget');
-register_widget_control('Syndication', 'syndication_widget_options', 350, 120);
+}
 
 ?>
